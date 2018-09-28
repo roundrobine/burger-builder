b9c966f075b47f7d3a3e0c185d17d0b922ab0345
Dimitar Goshevski <dimitar.goshevski@leoventures.com>
2018-09-28 18:43:56 +0200

    move BurgerBuilder to containers folder

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBullder extends Component {
   state = {};
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controles</div>
       </Aux>
     );
