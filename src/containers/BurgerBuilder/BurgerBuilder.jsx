776a7fdad38e4140ca44f02954f11e3b6657d31e
Dimitar Goshevski <dimitar.goshevski@leoventures.com>
2018-10-14 12:17:39 +0200

    add global axios error handler

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -142,4 +143,4 @@ class BurgerBullder extends Component {
   }
 }
 
-export default BurgerBullder;
+export default withErrorHandler(BurgerBullder, axios);
