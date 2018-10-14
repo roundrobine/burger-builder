1b8f8da0a91bd6660217d1940bacb32625967bd5
Dimitar Goshevski <dimitar.goshevski@leoventures.com>
2018-10-14 11:14:07 +0200

    send a new order to firebase using axios

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -75,7 +76,27 @@ class BurgerBullder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!!!");
+    // alert("You continue!!!");
+    const order = {
+      ingredinets: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Dimitar Goshevski",
+        street: "Paulo Debono 14",
+        zipCode: "2330MSD",
+        country: "Malta"
+      },
+      email: "roundrobine@gmail.com",
+      deliveryMethod: "fast"
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   render() {
